e7371fdbf8098417713c3c1f673fd40d40a4593e
petr <stechy12@gmail.com>
2017-08-29 13:48:45 +0200

    #7 Otestování ScreenPartManagera.

--- src/test/java/cz/stechy/screens/simple/controller/Controller1.java
+++ src/test/java/cz/stechy/screens/simple/controller/Controller1.java
@@ -3,15 +3,41 @@ package cz.stechy.screens.simple.controller;
 import cz.stechy.screens.BaseController;
 import cz.stechy.screens.Bundle;
 import cz.stechy.screens.Notification;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.StackPane;
 
 
-public class Controller1 extends BaseController {
+public class Controller1 extends BaseController implements Initializable {
+
+    public StackPane partContainer;
+    public ToggleButton toggleButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        toggleButton.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                getPartManager().inContainer(partContainer).onLoaded(
+                    (child, controller) -> System.out.println("Loaded 2")).show("part2");
+            } else {
+                getPartManager().inContainer(partContainer).onLoaded(
+                    (child, controller) -> System.out.println("Loaded 1")).show("part1");
+            }
+        });
+    }
 
     @Override
     protected void beforeShow() {
         setTitle("Prvni okno");
         setScreenSize(500, 400);
+    }
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        getPartManager().inContainer(partContainer).onLoaded(child, Object controller -> System.out.println("Loaded")).show("part1");
     }
 
     public void goTo2(ActionEvent actionEvent) {
